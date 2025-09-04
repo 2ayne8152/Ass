8bd045ce0b2c009867a119061bb6e6c52159cfd7
Wayne <waynechenyik@gmail.com>
2025-09-04 16:55:48 +0800

    fix jam plan

--- Ass/Main.cpp
+++ Ass/Main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <Windows.h>
+
 #include "login.h"
 #include "user.h"
 
 using namespace std;
 int main() {
+	SetConsoleOutputCP(CP_UTF8);
 	homePageMenu();
 	return 0;
 }
