1d5cc038263fb68b906cd8d7c182b360f87659f5
Wayne <waynechenyik@gmail.com>
2025-09-08 17:03:39 +0800

    cleaning

--- Ass/events.h
+++ Ass/events.h
@@ -12,19 +12,20 @@ struct Event {
     string startTime;
     string endTime;
     string status;
+    double venueFee;
     string paymentStatus;
     string organizerName;
-    double ticketPrice;         // <-- Added
-    int availableTickets;       // <-- Added
+    double ticketPrice;
+    int availableTickets;
 
     Event() = default;
 
     Event(int _id, string _name, string _venue, string _date, string _startTime, string _endTime,
-        string _status, string _organizerName, double _ticketPrice, int _availableTickets,
+        string _status, double _venueFee, string _organizerName, double _ticketPrice, int _availableTickets,
         string _paymentStatus = "Unpaid")
         : id(_id), name(_name), venue(_venue), date(_date),
         startTime(_startTime), endTime(_endTime),
-        status(_status), organizerName(_organizerName),
+        status(_status), venueFee(_venueFee), organizerName(_organizerName),
         ticketPrice(_ticketPrice), availableTickets(_availableTickets),
         paymentStatus(_paymentStatus) {
     }
